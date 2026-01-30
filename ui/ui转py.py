ff08c32698ff8c8715664dfb861651eda0da15e9
Piclaite <2928627563@qq.com>
2026-01-31 04:49:57 +0800

    Delete ui directory

--- ui/ui转py.py
+++
@@ -1,6 +0,0 @@
-import os
-wjm=str(input("ui文件路径："))
-pywjm=str(input("生成的py文件路径："))
-pyui5lj=r'D:\Python031402\Scripts\pyuic5.exe'
-pyui5lj=str(input(r"pyuic5路径（默认D:\Python031402\Scripts\pyuic5.exe）：") or pyui5lj )
-os.system(f"{pyui5lj} -o {pywjm} {wjm}")
